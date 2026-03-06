877a4e076cc5d46d7ff52c4699920916984059ab
Tom-Mendy <tom.mendy@epitech.eu>
2026-03-06 16:39:52 +0900

    feat(endfield): add initial Helm chart and Docker support for Endfield check-in automation

--- kubernetes/test-helm-chart.sh
+++ kubernetes/test-helm-chart.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-charts=(blocky homepage traefik keel prometheus grafana navidrome vaultwarden forgejo searxng)
+charts=(blocky homepage traefik keel prometheus grafana navidrome vaultwarden forgejo searxng endfield)
 for c in "${charts[@]}"
 do echo "=== $c ==="
 helm template test "$c" >/tmp/helm_render.out 2>/tmp/helm_render.err
